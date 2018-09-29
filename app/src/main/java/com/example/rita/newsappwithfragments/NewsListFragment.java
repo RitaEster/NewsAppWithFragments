7e2698acebc7f4c826ba3a5ea1a3ad775de1bdcc
RitaEster <RitaEster@coquitlamcollege.com>
2018-09-28 22:48:54 -0700

    List View in Main Activity (not clickable yet)

--- app/src/main/java/com/example/rita/newsappwithfragments/NewsListFragment.java
+++ app/src/main/java/com/example/rita/newsappwithfragments/NewsListFragment.java
@@ -1,29 +1,34 @@
 package com.example.rita.newsappwithfragments;
 
-
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ArrayAdapter;
 
 /**
- * A simple {@link Fragment} subclass.
+ * A simple {@link ListFragment} subclass.
  */
-public class NewsListFragment extends Fragment {
-
+public class NewsListFragment extends ListFragment {
 
     public NewsListFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_list, container, false);
-    }
 
+        // create an array adapter for the headlines.
+        // the context of a fragment is provided by the inflater.
+        ArrayAdapter<String> headlinesAdapter = new ArrayAdapter<String>
+                (inflater.getContext(), android.R.layout.simple_list_item_1,News.getHeadlines());
+
+        setListAdapter(headlinesAdapter);
+
+        // inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_news_list, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
